facb9973cc85673440ef5e2f5b19f67f0d317ceb
popobo <331361996@qq.com>
2023-05-09 18:40:23 +0800

    pratice 51 led and button

---
+++ 3_button/main.c
@@ -0,0 +1,89 @@
+#include <REGX52.H>
+
+void Delay_ms(unsigned int ms)		//@12.000MHz
+{
+	while(ms--)
+	{
+		unsigned char i, j;
+
+		i = 2;
+		j = 239;
+		do
+		{
+			while (--j);
+		} while (--i);
+	}
+}
+
+void main()
+{
+
+	/*
+	while(1)
+	{
+		P2_0 = P3_1;
+	}
+	*/
+	
+	/*
+	while(1)
+	{
+		if (!P3_1)
+		{
+			Delay_ms(20);
+			while (!P3_1);
+			Delay_ms(20);
+			P2_0 = ~P2_0;
+		}
+	}
+	*/
+	
+	
+	/*
+	unsigned char led_num = 0;
+	
+	while(1)
+	{
+		if (!P3_1)
+		{
+			Delay_ms(20);
+			while (!P3_1);
+			Delay_ms(20);
+			led_num++;
+			P2 = ~led_num;
+		}
+	}
+	*/
+	
+	unsigned char led_num = 1;
+	
+	while(1)
+	{
+		if (!P3_1)
+		{
+			Delay_ms(20);
+			while (!P3_1);
+			Delay_ms(20);
+			led_num = led_num << 1;
+			if (led_num == 0)
+			{
+				led_num = 1;
+			}
+			P2 = ~led_num;
+		}
+		
+		if (!P3_0)
+		{
+			Delay_ms(20);
+			while (!P3_0);
+			Delay_ms(20);
+			led_num = led_num >> 1;
+			if (led_num == 0)
+			{
+				led_num = 0x80;
+			}
+			P2 = ~led_num;
+		}
+	}
+	
+}
